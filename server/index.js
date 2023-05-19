f1b33f4e1a520fa2c0d9329fb8e4cf3b2606b048
brandongiordano <bgiordano9917@aol.com>
2023-05-19 13:28:19 -0400

    user auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,8 @@ const storage = multer.diskStorage({
     }
 });
 const uplaod = multer({ storage });
+
+app.post('/auth/register', uplaod.single("picture"), register);
 
 // Mongoose
 const PORT = process.env.PORT || 6001;
