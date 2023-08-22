f67577db59ae8706e0d20e65f80c7a2671ab36fe
brandongiordano <bgiordano9917@aol.com>
2023-08-22 12:45:43 -0400

    corrected typo

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -22,7 +22,7 @@ export const authSlice = createSlice({
             state.user = null;
             state.token = null;
         },
-        setFroemds: (state, action) => {
+        setFriends: (state, action) => {
             if (state.user) {
                 state.user.friends = action.payload.friends;
             } else {
